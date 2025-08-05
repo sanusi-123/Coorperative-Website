b5807f6e620f48371796d93d43cd023b530c1f55
sanusi-123 <sanusiridwan156@gmail.com>
2025-08-05 16:52:16 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Toggle mobile menu
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('navMenu');
+
+hamburger.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+});
